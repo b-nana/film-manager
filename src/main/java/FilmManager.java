71ef402e3a54bdc0293c60c1baf0b88f5a8c46d2
b-nana <babenkoaa97@gmail.com>
2022-08-15 15:50:37 +0300

    Initial commit

---
+++ src/main/java/FilmManager.java
@@ -0,0 +1,49 @@
+public class FilmManager {
+    private String[] films = new String[0];
+
+    public void add(String film) {
+        String[] tmp = new String[films.length + 1];
+
+    for (int i = 0; i < films.length; i++) {
+        tmp[i] = films[i];
+    }
+    tmp[tmp.length - 1] = film;
+    films = tmp;
+    }
+
+    public String[] findAll() {
+    return films;
+    }
+
+    public String[] findLast10() {
+        String[] all = findAll();
+        int count;
+        if (all.length >= 10) {
+            count = 10;
+        } else {
+            count = all.length;
+        }
+        String[] reversed = new String[count];
+        for (int i = 0; i < count; i++) {
+            reversed[i] = all[all.length - 1 - i];
+        }
+        return reversed;
+    }
+
+    public String[] findLastN(int count) {
+        String[] all = findAll();
+        int newLength;
+        if (count <= all.length) {
+            newLength = count;
+        } else {
+            newLength = all.length;
+        }
+        String[] reversed = new String[newLength];
+        for (int i = 0; i < newLength; i++) {
+            reversed[i] = all[all.length - 1 - i];
+        }
+        return reversed;
+    }
+}
+
+
---
+++ src/test/java/FilmManagerTest.java
@@ -0,0 +1,109 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FilmManagerTest {
+
+
+    @Test
+    public void shouldAdd() {
+        FilmManager manager = new FilmManager();
+        manager.add("film1");
+        manager.add("film2");
+        manager.add("film3");
+
+        String[] expected = {"film1", "film2", "film3"};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+
+    public void shouldFindLast10InReverse() {
+        FilmManager manager = new FilmManager();
+        manager.add("film1");
+        manager.add("film2");
+        manager.add("film3");
+        manager.add("film4");
+        manager.add("film5");
+        manager.add("film6");
+        manager.add("film7");
+        manager.add("film8");
+        manager.add("film9");
+        manager.add("film10");
+        manager.add("film11");
+
+        String[] expected = {"film11", "film10", "film9", "film8", "film7", "film6", "film5", "film4", "film3", "film2" };
+        String[] actual = manager.findLast10();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+
+    @Test
+
+    public void shouldFindLastNInReverse() {
+        FilmManager manager = new FilmManager();
+        int count = 5;
+        manager.add("film1");
+        manager.add("film2");
+        manager.add("film3");
+        manager.add("film4");
+        manager.add("film5");
+        manager.add("film6");
+        manager.add("film7");
+        manager.add("film8");
+        manager.add("film9");
+        manager.add("film10");
+        manager.add("film11");
+
+        String[] expected = {"film11", "film10", "film9", "film8", "film7"};
+        String[] actual = manager.findLastN(count);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+
+    @Test
+
+    public void findLast10IfLessThan10() {
+
+        FilmManager manager = new FilmManager();
+        manager.add("film1");
+        manager.add("film2");
+        manager.add("film3");
+        manager.add("film4");
+        manager.add("film5");
+        manager.add("film6");
+        manager.add("film7");
+        manager.add("film8");
+        manager.add("film9");
+
+        String[] expected = {"film9", "film8", "film7", "film6", "film5", "film4", "film3", "film2", "film1"};
+        String[] actual = manager.findLast10();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void findLastNIfLessThanN() {
+
+        FilmManager manager = new FilmManager();
+        int count = 5;
+        manager.add("film1");
+        manager.add("film2");
+        manager.add("film3");
+        manager.add("film4");
+
+        String[] expected = {"film4", "film3", "film2", "film1"};
+        String[] actual = manager.findLastN(count);
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+}
+
